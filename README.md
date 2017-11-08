5a1eb72475043d58d8cf49cb23c8b960ef48485c
Andrew Nguyen <drew.nguyen5@gmail.com>
2017-11-08 07:45:58 -0500

    Added styling, a couple API points, and changed demo to todo list

---
+++ README.md
@@ -0,0 +1,5 @@
+# DocQuery
+
+DocQuery is a DOM manipulation library. To compile, use "webpack --watch" in the root directory.
+
+It features an API capable of making basic changes to the DOM such as adding children and setting attributes as well as AJAX calls which return promise objects.
--- js/demo.js
+++ js/demo.js
@@ -1,14 +1,34 @@
+$(() => {
+  const newTodoInput = $('.new-todo');
+  const addTodoButton = $('button');
+  const todoList = $('.todo-list');
+
+  addTodoButton.on("click", () => {
+    const todoItem = $("<li>");
+    todoItem.html(newTodoInput.val());
+    todoList.append(todoItem);
+    newTodoInput.val("");
+  });
+
+  getWeather();
+});
+
 const getWeather = () => {
-  $.ajax({
+  return $.ajax(
+    {
       type: 'GET',
-      url: "http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=bcb83c4b54aee8418983c2aff3073b3b",
+      url: "http://api.openweathermap.org/data/2.5/weather?q=NewYork,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
       success(data) {
+        console.log("We have your weather!");
         console.log(data);
+        const temp = Math.floor((data.main.temp - 273.15) * 9 / 5 + 32);
+        $(".temperature").append(temp + "°F");
       },
       error() {
         console.error("An error occurred.");
       },
-   });
+    }
+  );
 };
 
-getWeather();
+window.getWeather = getWeather;
