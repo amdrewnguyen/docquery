3f6a1c51dbef567ff051a8a663890546a218243a
Andrew Nguyen <drew.nguyen5@gmail.com>
2017-11-10 11:43:20 -0500

    Added localstorage persistence for todo items. Styled page to showcase widgets

--- js/demo.js
+++ js/demo.js
@@ -3,15 +3,63 @@ $(() => {
   const addTodoButton = $('button');
   const todoList = $('.todo-list');
 
+  const myStorage = window.localStorage;
+
+  let itemString = myStorage.getItem("todoItems");
+  let todoItems;
+  if (itemString) {
+    todoItems = JSON.parse(itemString);
+  } else {
+    todoItems = [];
+  }
+
+  todoItems.forEach(
+    (item) => {
+      let todoLi = newTodoLi(item);
+      todoList.append(todoLi);
+    }
+  );
+
   addTodoButton.on("click", () => {
-    const todoItem = $("<li>");
-    todoItem.html(newTodoInput.val());
+    let inputText = newTodoInput.val();
+    const todoItem = newTodoLi(inputText);
     todoList.append(todoItem);
+    todoItems = saveNewTodo(myStorage, inputText, todoItems);
     newTodoInput.val("");
   });
 
-  getWeather();
+  todoList.on("click", (e) => {
+    // console.log(e.target.parentNode);
+    if (e.target.classList.contains("remove-btn")) {
+      // console.log("delete clicked");
+      e.currentTarget.removeChild(e.target.parentNode);
+      let newTodos = $(".todo-text");
+      myStorage.setItem("todoItems", JSON.stringify(newTodos.text()));
+    }
+  });
+
+  // getWeather();
 });
+
+
+const newTodoLi = (text) => {
+  const todoItem = $("<li>");
+  const textSpan = $("<span>");
+  textSpan.addClass("todo-text");
+  textSpan.html(text);
+  const removeButton = $("<button>");
+  removeButton.addClass("remove-btn");
+  removeButton.text("X");
+  todoItem.append(removeButton);
+  todoItem.append(textSpan);
+  return todoItem;
+};
+
+const saveNewTodo = (store, newTodo, todos) => {
+  const newTodos = todos.concat(newTodo);
+  store.setItem("todoItems", JSON.stringify(newTodos));
+  return newTodos;
+};
 
 const getWeather = () => {
   return $.ajax(
@@ -21,14 +69,23 @@ const getWeather = () => {
       success(data) {
         console.log("We have your weather!");
         console.log(data);
+        let d = new Date();
+        $(".date").append(formatDateString(d.toDateString()));
+        $(".desc").append(data.weather[0].main);
         const temp = Math.floor((data.main.temp - 273.15) * 9 / 5 + 32);
         $(".temperature").append(temp + "°F");
+
       },
       error() {
         console.error("An error occurred.");
       },
     }
   );
+};
+
+const formatDateString = (s) => {
+  let words = s.split(" ");
+  return `${words[0]}, ${words[1]} ${words[2]}`;
 };
 
 window.getWeather = getWeather;
